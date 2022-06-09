6582ea08b3ca3f6c2289362254daa837e3386a6d
Gaurav <allmanpride@gmail.com>
2022-06-09 18:31:28 -0400

    Add demo video

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 import os
+import random
+import time
 from multiprocessing import Process
 
 N = 4
 
 
 def process_test(bi):
-    total = 1000
+    total = 100
     for i in range(total):
         os.system(f'dmon-send p-{bi} {bi} {(i+1)/total*100} > /dev/null')
+        time.sleep(random.random()/10)
 
 
 procs = [Process(target=process_test,
